85a4068b02f6e4c4e0048bb2b1fef54d312ac4a0
alanholguin21 <144397855+alanholguin21@users.noreply.github.com>
2024-09-14 21:02:13 -0600

    Testing Page added.

---
+++ frontend/src/routes/Testing/+page.svelte
@@ -0,0 +1,12 @@
+<script>    
+    import Navbar from '$lib/navbar.svelte';
+    import CircularProgress from '../../lib/CircularProgress.svelte'; // Import any components needed for the testing page
+    import TestResults from '../../lib/TestResults.svelte'; 
+    import ScheduleAttacks from '../../lib/ScheduleAttacks.svelte'; 
+    import Summary from '../../lib/Summary.svelte';
+</script>
+<Navbar />
+<CircularProgress />
+<TestResults />
+<ScheduleAttacks />
+<Summary />
