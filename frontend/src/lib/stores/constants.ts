49cfb8c5032053e3714795f0ac202b18833c1178
russell <lcedillo@miners.utep.edu>
2024-12-06 07:11:53 -0700

    wait

--- frontend/src/lib/stores/constants.ts
+++
@@ -1,9 +0,0 @@
-export const EXPLOITS = [
-    "Unauthenticated port bypass",
-    "Default credentials",
-    "Missing encryption protocols",
-    "Unpatched software exploits",
-    "Other",
-] as const;
-
-export type ExploitType = typeof EXPLOITS[number];
